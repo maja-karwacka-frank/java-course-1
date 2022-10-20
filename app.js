c9efbb48c55a5b58649307b364842c7161558d37
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2022-10-20 11:31:15 +0200

    Let added to app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log(99+99);
+console.log(2*10*3.1415);
+console.log(2*50*3.1415);
+console.log(2*7*3.1415);
+console.log(2*4*3.1415);
+
+let pi = 3.14;
+console.log(2*10*pi)
+
+let C = 299792458;
+let M = 10;
+let E = M * C * C;
+console.log(E);
