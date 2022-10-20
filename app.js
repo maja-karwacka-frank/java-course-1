e0f15926f31dddba30571256d2d2ee939a33b766
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2022-10-20 14:11:00 +0200

    New function added in app.js

--- app.js
+++ app.js
@@ -1,33 +1,18 @@
-console.log(99+99);
-console.log(2*10*3.1415);
-console.log(2*50*3.1415);
-console.log(2*7*3.1415);
-console.log(2*4*3.1415);
+let arrowLeft = document.querySelector(".arrow-left");
+let arrowRight = document.querySelector(".arrow-right");
 
-let pi = 3.14;
-console.log(2*10*pi)
+let dot1 = document.querySelector("#dot1");
+let dot2 = document.querySelector("#dot2");
+let dot3 = document.querySelector("#dot3");
 
-let C = 299792458;
-let M = 10;
-let E = M * C * C;
-console.log(E);
+let slide1 = document.querySelector("#slide1");
+let slide2 = document.querySelector("#slide2");
+let slide3 = document.querySelector("#slide3");
 
-let name = "Maja";
-let lastName = "Karwacka-Frank";
-console.log(name + " " + lastName);
+let showSlide2 = () => {
+    let activeElement = document.querySelector(".active");
+    activeElement.classList.remove("active");
+    slide2.classList.add("active");
+};
 
-name = "Jan";
-lastName = "Kowalski";
-console.log(name + " " + lastName);
-
-let person1 = {
-    name: "Maja",
-    lastName: "Karwacka-Frank"
-}
-console.log(person1.name + " " + person1.lastName);
-
-let person2 = {
-    name: "Jan",
-    lastName: "Kowalski"
-}
-console.log(person2.name + " " + person2.lastName)
+dot2.addEventListener("click", showSlide2);
---
+++ examples.js
@@ -0,0 +1,23 @@
+console.log("Examples work!");
+let myFirstFunction = (name, greeting) => {
+    console.log(greeting + " mam na imię " + name + "!");
+};
+myFirstFunction("Maja", "Hej");
+myFirstFunction("Wojciech", "Dzień dobry");
+myFirstFunction("Raymond", "Czaisz");
+myFirstFunction("Mietek", "Siemka");
+
+let multiplyBy2 = (num) => {
+    console.log(num*2);
+}
+multiplyBy2(4);
+
+let multiply = (a, b) => {
+    console.log(a * b)
+};
+multiply(2,5);
+
+let squere = (num) => {
+    console.log(num * num);
+};
+squere(4);
